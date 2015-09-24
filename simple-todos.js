15e0ed0c0ccdbad07780a6eb9ce2e89a968b774d
rohit2b <hadoopian@gmail.com>
2015-09-24 16:00:51 -0700

    Completed step 4 with comments

--- simple-todos.js
+++ simple-todos.js
@@ -8,7 +8,40 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+      // This commented out statement returns list in order stored in array
+      // return Tasks.find({});
+    }
+  });
+ 
+  Template.body.events({
+    // Add to listen to the submit event on the form.
+    // We are listening to the submit event on any element that matches 
+    // the CSS selector .new-task.
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // event.target is our form element.
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Inspect event object on console
+      console.log(event);
+
+      // Insert a task into the collection
+      // Assign any properties to the task object, such as the time created, 
+      // since we don't ever have to define a schema for the collection.
+      // Being able to insert anything into the database from the client isn't 
+      // very secure.
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form to prepare for another new 
+      event.target.text.value = "";
     }
   });
 }
